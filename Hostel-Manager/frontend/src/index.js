9b0755d9321af3ea5ad573e057507b6adaa23055
rohangupta513 <127219893+rohangupta513@users.noreply.github.com>
2024-02-15 15:25:38 +0530

    Add files

---
+++ Hostel-Manager/frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/index.css'
+import { Provider } from 'react-redux'
+import store from './store'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import './css/bootstrap.min.css'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
+
+reportWebVitals()
